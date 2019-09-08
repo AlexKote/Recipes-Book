509e5fa28cf7c4973470eb59e8cda48336965100
Alex <shpil9159@gmail.com>
2019-09-08 15:36:44 +0300

    Authentication

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -3,18 +3,24 @@ import { HttpClient } from '@angular/common/http';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipes.model';
 import { map } from 'rxjs/operators';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable()
 export class DataStorageService {
     constructor(private http: HttpClient, 
-                private recipeService: RecipeService) {}
+                private recipeService: RecipeService,
+                private authService: AuthService) {}
 
-    storeRecipes() {
-        return this.http.put('https://ng-recipe-book-c5b66.firebaseio.com/recipes.json', this.recipeService.getRecipes());
+    storeRecipes() {        
+        const token = this.authService.getToken();
+        
+        return this.http.put('https://ng-recipe-book-c5b66.firebaseio.com/recipes.json?auth=' + token, this.recipeService.getRecipes());
     }
 
     getRecipes() {
-        this.http.get('https://ng-recipe-book-c5b66.firebaseio.com/recipes.json').pipe(
+        const token = this.authService.getToken();
+        
+        this.http.get('https://ng-recipe-book-c5b66.firebaseio.com/recipes.json?auth=' + token).pipe(
             map(
                 (recipes: Recipe[]) => {
                     for (let recipe of recipes) {
