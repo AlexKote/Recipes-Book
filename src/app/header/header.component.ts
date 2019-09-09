dbdffc014a59d5b5c1bb27f911e5835c59c353f6
Alex <shpil9159@gmail.com>
2019-09-09 15:17:13 +0300

    Add ingridients count

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DataStorageService } from '../shared/data-storage.service';
 import { AuthService } from '../auth/auth.service';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { Subscription } from 'rxjs';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy{
+    private subscription: Subscription;    
+    ingredientCount = this.slservice.getIngredients().length;
+
     constructor(private dataStorageService: DataStorageService,
-                private authService: AuthService) {}
+                private authService: AuthService,
+                private slservice: ShoppingListService) {}
+
+    ngOnInit() {
+        this.subscription = this.slservice.ingredientsChanged.subscribe(
+          (ingredients: Ingredient[]) => {
+            this.ingredientCount = ingredients.length;
+          }
+        );  
+    }
 
     onSaveData() {
         this.dataStorageService.storeRecipes().subscribe(
@@ -25,4 +40,8 @@ export class HeaderComponent {
     onLogout() {
         this.authService.logout();
     }
+
+    ngOnDestroy() {
+        this.subscription.unsubscribe();
+      }
 }
